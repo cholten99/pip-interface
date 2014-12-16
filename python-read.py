377cc410e6cfc69e4aec566e82383fb84623c36b
David Durant <dave@bowsy.co.uk>
2014-12-15 20:16:48 -0500

    All change tomorrow

--- python-read.py
+++ python-read.py
@@ -4,7 +4,7 @@ import pymongo
 from bson.objectid import ObjectId
 import time
 # Setup callback to access mongoDB
-client = pymongo.MongoClient("localhost", 27017)
+client = pymongo.MongoClient("bowsy.co.uk", 27017)
 db = client.pipinterface
 def callback(ch, method, properties, body):
   # Ackowledge RabbitMQ
@@ -14,8 +14,8 @@ def callback(ch, method, properties, body):
   pip_entry = db.pipinterface.find( { "_id": uid_object } )[0]
   # Brief pause
   db.pipinterface.update({"_id": uid_object}, {"$set":{"ProcStatus": "Processing"}})
-  print " [x] Received %r" % (pip_entry,) + " -- Sleeping " + str(random_pause) + " seconds"
   random_pause = pip_entry["WaitTime"]
+  print " [x] Received %r" % (pip_entry,) + " -- Sleeping " + str(random_pause) + " seconds"
   time.sleep(random_pause)
   # Set final processing status
   db.pipinterface.update({"_id": uid_object}, {"$set":{"ProcStatus": "Processed"}})
